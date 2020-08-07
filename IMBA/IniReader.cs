fa6af9ec23e6555f11220461fe3475738856ca11
Aleksandr Penkovski <assassin.of.zero@gmail.com>
2020-08-07 15:26:07 +0300

    Добавьте файлы проекта.

---
+++ IMBA/IniReader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IMBA
+{
+    public class IniReader
+    {
+        public string[] iniText { get; private set; }
+        private string Path2Ini { get; set; }
+
+        public IniReader(string path)
+        {
+            iniText = File.ReadAllLines(path);
+            Path2Ini = path;
+        }
+
+        public string GetValuebyParam(string paramName)
+        {
+            iniText = File.ReadAllLines(Path2Ini);
+
+            foreach(var i in iniText)
+            {
+                if (i.StartsWith(paramName + "="))
+                    return i.Remove(0, paramName.Length + 1);
+            }
+            return null;
+        }
+
+        public string[] GetValuebyParam(string paramName, bool isArray)
+        {
+            iniText = File.ReadAllLines(Path2Ini);
+
+            if(isArray)
+            {
+                string[] a;
+
+                for(int i = 0; i<iniText.Length; i++)
+                {
+                    if(iniText[i].StartsWith(paramName + "=array"))
+                    {
+                        int b = 0;
+                        a = new string[iniText.Skip(i + 2).TakeWhile(x => x != "}").Count()];
+                        int c = i + 2 + a.Length;
+                        for(int j = i+2; j < c; j++)
+                        {
+                            a[b] = new Regex(@".*=").Replace(iniText[j], "");
+                            b++;
+                        }
+                        return a;
+                    }
+                }
+                return null;
+            }
+            else
+            {
+                return new string[] { GetValuebyParam(paramName) };
+            }
+        }
+    }
+}
---
+++ IMBA/IniWriter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMBA
+{
+    public class IniWriter
+    {
+        public List<string> iniText { get; private set; }
+        private string Path2Ini { get; set; }
+
+        public IniWriter(string path)
+        {
+            if (!File.Exists(path))
+                File.Create(path).Close();
+            iniText = File.ReadAllLines(path).ToList();
+            Path2Ini = path;
+        }
+
+        public bool WriteParam(string paramName, string paramValue)
+        {
+            try
+            {
+                for (int i = 0; i < iniText.Count; i++)
+                {
+                    if (iniText[i].StartsWith(paramName + "="))
+                    {
+                        iniText[i] = paramName + "=" + paramValue;
+                        WriteChanges();
+                        return true;
+                    }
+                }
+                iniText.Add(paramName + "=" + paramValue);
+                WriteChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool WriteParam(string paramName, string[] paramValues)
+        {
+            try
+            {
+                for (int i = 0; i < paramValues.Length; i++)
+                    paramValues[i] = $@"{i}={paramValues[i]}";
+                for(int i = 0; i < iniText.Count; i++)
+                {
+                    if(iniText[i].StartsWith(paramName + "=array"))
+                    {
+                        iniText.RemoveRange(i, iniText.Skip(i + 2).TakeWhile(x => x != "}").Count() + 3);
+
+                        List<string> abs = paramValues.ToList();
+                        abs.Add("}");
+                        abs.Insert(0, "{");
+                        abs.Insert(0, $"{paramName}=array");
+                        iniText.InsertRange(i, abs);
+                        WriteChanges();
+                        return true;
+                    }
+                }
+                List<string> list = paramValues.ToList();
+                list.Add("}");
+                list.Insert(0, "{");
+                list.Insert(0, $"{paramName}=array");
+                iniText.AddRange(list);
+                WriteChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void WriteChanges()
+        {
+            File.WriteAllLines(Path2Ini, iniText);
+        }
+    }
+}
